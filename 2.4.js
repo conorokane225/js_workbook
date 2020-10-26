5e6ff8a2927362c67a52611d03b7bd7d8f2aea31
conorokane225 <71731584+conorokane225@users.noreply.github.com>
2020-10-26 08:52:57 -0500

    changes to js workbook
    changes to js workbook

---
+++ 2.4.js
@@ -0,0 +1,11 @@
+let admin;
+let name;
+name= "John";
+admin= name;
+alert(admin);
+
+let ourPlanet="Earth";
+let currentUserName="Conor";
+
+//const BIRTHDAY should be capitalized becasue the birtday date will never change
+//const age should be lowercase because it will change with every birthday that passes
